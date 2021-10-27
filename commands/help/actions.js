ff2e4e9affa99c67402aec1b43416f879eae7297
GIMENEZ TANGUY <tanguy.gimenez@etu.unistra.fr>
2021-10-27 21:19:02 +0200

    feat/command : ajout de la commande help

---
+++ commands/help/actions.js
@@ -0,0 +1,66 @@
+const Discord = require("discord.js");
+const process = require("process");
+
+function init(client) {
+	const allCommands = client.commands.keys();
+
+	for (const cmdName of allCommands) {
+		this.options[0].choices.push({
+			name: cmdName,
+			value: cmdName
+		});
+	}
+
+	this.color = process.env.COLOR;
+}
+function shutdown() {
+
+}
+async function execute(interaction, options) {
+	const embed = new Discord.MessageEmbed();
+	embed.setColor(this.color);
+
+	if (options.length === 0) {
+		embed.setTitle("L'aide du Palila Bot !");
+
+		for (const cmd of interaction.client.commands.values()) {
+			embed.addField(cmd.name, cmd.description);
+		}
+	}
+	else {
+		const commandName = options[0].value;
+
+		const cmd = interaction.client.commands.get(commandName);
+
+		embed.setTitle(cmd.name);
+		embed.addField("Description", cmd.description);
+		embed.addField("Explication", cmd.explication);
+		embed.setFooter(`Commande créée par ${cmd.author}`);
+
+		if (cmd.subcommands) {
+			let subCommands = "";
+
+			cmd.subcommands.forEach(subcmd => {
+				subCommands += `**${subcmd.name}** : ${subcmd.description}\n`;
+			});
+
+			embed.addField("Options", subCommands);
+		}
+	}
+
+
+	interaction.reply({ embeds: [embed] });
+}
+async function middleware() {
+
+}
+async function configure() {
+
+}
+module.exports = {
+	init,
+	shutdown,
+	execute,
+	middleware,
+	configure,
+};
---
+++ commands/help/infos.js
@@ -0,0 +1,16 @@
+module.exports = {
+	name: "help",
+	description: "L'aide de ce bot",
+	explication: "Cette commande affiche l'aide du bot et les explications des commandes du bot.",
+	author: "Kayn",
+	options: [{
+		name: "commande",
+		type: "STRING",
+		description: "Nom de la commande dont vous voulez voir l'aide",
+		required: false,
+		choices: []
+	}],
+	intents: [],
+	permissions: [],
+	configurations: [],
+};
