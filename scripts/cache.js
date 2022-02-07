2c3bf1b249fdfaa1382b2209d3ff1f49bc1c3ce3
Tanguy Gimenez <gimeneztanguy@gmail.com>
2022-02-07 18:31:04 +0100

    feat/dev: adding cache updater

---
+++ scripts/cache.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const axios = require("axios");
+const process = require("process");
+const { LocalCache } = require("../services/cache");
+
+const cache = LocalCache.get("lynch");
+const LYNCH_CHANNEL_ID = "UUDLD_zxiuyh1IMasq9nbjrA";
+
+
+axios({
+	method: "GET",
+	url: "https://youtube.googleapis.com/youtube/v3/playlistItems",
+	params: {
+		part: "snippet",
+		maxResults: 50,
+		key: process.env.YOUTUBE_API_KEY,
+		playlistId: LYNCH_CHANNEL_ID
+	}
+}).then( videos => {
+	cache.set("history", videos.data.items.map( video => video.snippet.resourceId.videoId ));
+	console.log("- Cache lynch set !");
+});
